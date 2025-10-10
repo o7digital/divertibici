dbf10b829c192778256334cf9fd2e9680fedacd0
o7digital <101292703+o7digital@users.noreply.github.com>
2025-10-09 22:35:42 -0600

    Initial setup for Divertibici home 16

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import HomePage16 from "./(homes)/home-16/page";
+
+export const metadata = {
+  title: "Home 16 || Uomo eCommerce React Nextjs Template",
+  description: "Uomo eCommerce React Nextjs Template",
+};
+export default function Home() {
+  return (
+    <>
+      <HomePage16 />
+    </>
+  );
+}
